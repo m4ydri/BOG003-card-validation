2c95367b71f731e05d38dab5dd4603dea21192f2
Mayerly Rodriguez <m4ydri>
2021-07-06 23:54:53 -0500

    Desarrollo de la interfaz principal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const validar = document.getElementById("buttonValidation");
+console.log(validar);
+validar.addEventListener("click",function(){
+    alert("hola");
+});
+
+
+
+
+
+
+function enviarvalidación(event){
+    event.preventDefault();
+   
+    button.innerText = `${button.innerText} $
+    {donation}`
+}
+
+
