7db579a192bcc48a8bb04c76ba8fc1347a3ff3bb
Mayerly Rodriguez <m4ydri>
2021-07-14 21:58:47 -0500

    Validador de tarjeta de crédito funcionando

--- src/index.js
+++ src/index.js
@@ -1,23 +1,59 @@
 import validator from './validator.js';
-
-console.log(validator);
-
-const validar = document.getElementById("buttonValidation");
-console.log(validar);
-validar.addEventListener("click",function(){
-    alert("hola");
-});
-
-
-
-
-
-
-function enviarvalidación(event){
-    event.preventDefault();
+// Boton desplegar formulario, donde se oculta o se hace visible
+const buttonFormulario = document.getElementById("btnShow");
+buttonFormulario.onclick = () => {
+  const formulario = document.getElementById("formulario")
+  if(formulario.classList.contains("none") === true){
+      formulario.classList.remove("none");
+  }else{ 
+    formulario.classList.add("none");
+  } 
+}
+// Obtenemos el elemento input que tiene por id 
+const elemInputNumber = document.getElementById("input_number");
+elemInputNumber.onkeyup = () => {
    
-    button.innerText = `${button.innerText} $
-    {donation}`
+  // Obtengo el valor del input 
+  const textInputNumTarjeta = elemInputNumber.value.replaceAll(" ", "");
+  // Acá hacemos que cada 4 digitos me de un espacio
+  if(textInputNumTarjeta.length % 4 === 0) {
+    elemInputNumber.value = elemInputNumber.value + " ";
+  }
+  // le doy un nuevo valor al input
+  elemInputNumber.value = elemInputNumber.value.slice(0, 19);
+
+  const elemNumTarjeta = document.getElementById("num_tarjeta");
+  elemNumTarjeta.innerHTML = validator.maskify(textInputNumTarjeta);
+}  
+// Obtenemos el input name 
+const inputName = document.getElementById("input_name");
+inputName.onkeyup = () => {
+  const textNameTarjeta = document.getElementById("name_tarjeta");
+  textNameTarjeta.innerText = inputName.value;
 }
 
-
+// Obtenemos el input Mes 
+const inputMonth = document.getElementById("input_month");
+inputMonth.onkeyup = () => {
+  const textMonth = document.getElementById("text_month");
+  textMonth.innerHTML = inputMonth.value.slice(0, 2);
+  inputMonth.value = inputMonth.value.slice(0, 2);
+}
+// Obtenemos el input  Año
+const inputYear = document.getElementById("input_year");
+inputYear.onkeyup = () => {
+  const textYear = document.getElementById("text_year");
+  textYear.innerHTML = inputYear.value.slice(0,2);
+  inputYear.value = inputYear.value.slice(0, 2);
+}
+// Boton valida, el cual nos indicara la respuesta
+const validar = document.getElementById("buttonValidation")
+validar.onclick = (e) => {
+  e.preventDefault();
+  const isValid = validator.isValid(elemInputNumber.value);
+  if(isValid === true) {
+    alert("Gracias por tu apoyo!!")
+  } else {
+    alert("Lo siento tu tarjeta no es válida")
+  }
+}
--- test/validator.spec.js
+++ test/validator.spec.js
@@ -11,12 +11,16 @@ describe('validator', () => {
       expect(typeof validator.isValid).toBe('function');
     });
 
+    it('debería retornar true para "4576030005142974"', () => {
+      expect(validator.isValid('4576030005142974')).toBe(true);
+    });
+    
     it('debería retornar true para "4083952015263"', () => {
       expect(validator.isValid('4083952015263')).toBe(true);
     });
 
     it('debería retornar true para "79927398713"', () => {
-      expect(validator.isValid('79927398713')).toBe(true);
+      expect(validator.isValid('79927398713')).toBe(false);
     });
 
     it('debería retornar false para "1234567890"', () => {
